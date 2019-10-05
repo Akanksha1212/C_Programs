949dacbbda8ce3c9c0b83c0756ef6583040f4241
Shreya Rastogi <43791350+ShreyaR14@users.noreply.github.com>
2019-10-05 11:56:48 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -25,12 +25,11 @@ int main()
 }
 int factorial(int num)
 {
-    if (num == 0 || num == 1)
+    if (num == 0)
     {
         return 1;
+        return num* factorial(num-1);
+
     }
-    else
-    {
-        return(num * factorial(num - 1));
-    }
+   
 }
