c6ce28fe7fdf37cd477503a0156dbabec416699c
bhavya1108 <bhavya.jain@students.iiit.ac.in>
2019-10-06 16:16:59 +0530

    i have entered fibonacci code

--- fibonacci.c
+++ fibonacci.c
@@ -1,18 +1,36 @@
-#include <stdio.h>
+#include<stdio.h>
+long long int mod=1e9 + 7;
+const int max =1e6 + 5;
+long long int f[1000005];
+long long int fib(long long int n)
+{
+	if(f[n] != -5) return f[n];
+	if(n==0)
+		return 0;
+	else if(n==1)
+		return 1;
+	else
+	{
+		f[n]=(fib(n-1)+fib(n-2))%mod;
+		return f[n];
+	}
+
+}
 int main()
 {
-    //This program displays the fibonacci series upto specified counter
-
-    int i, n, t1 = 0, t2 = 1, nextTerm;
-    printf("Enter the number of terms: ");
-    scanf("%d", &n);
-    printf("Fibonacci Series: ");
-    for (i = 1; i <= n; ++i)
-    {
-        printf("%d, ", t1);
-        nextTerm = t1 + t2;
-        t1 = t2;
-        t2 = nextTerm;
-    }
-    return 0;
+	long long int n;
+	int t;
+	scanf("%d",&t);
+		for(int i=0;i<max;i++)
+			f[i]=-5;
+		while(t--)
+	
+	{
+		scanf("%lld",&n);
+		printf("%lld\n",fib(n));
+		
+	}
+	return 0;
+	
 }
+
