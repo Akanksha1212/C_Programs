003126ede5c2cd8ea9a792d50397bee456494d3f
NILANJAN TARAFDER <nilanjantarafder10@gmail.com>
2019-10-09 19:33:10 +0530

    Update 2d-matrix.c

--- basic_programs/2d-matrix.c
+++ basic_programs/2d-matrix.c
@@ -2,41 +2,26 @@
 
 int main()
 {
-    int n, i, j, t;
-
-    printf("Enter the value of n: ");
-    scanf("%d", &n);
-
-    t = 2 * n - 1;
-    i = t; 
-    j = t;
-
-    int a[i][j], k, m, p;
-    p = n;
-    m = 0;
-
-    for (k = 0; k < p; k++) {
-        for (i = m; i < t; i++) {
-            for (j = m; j < t; j++) {
-                if (i == m || i == (t - 1) || j == m || j == (t - 1)) {
-                    a[i][j] = n;
-                    if (n == 1) {
-                        break;
-                    }
-                }
-            }
-        }
-        t = t - 1;
-        n = n - 1;
-        m = m + 1;
-    }
-    t = 2 * m - 1;
-    for (i = 0; i < t; i++) {
-        for (j = 0; j < t; j++) {
-            printf("%d ", a[i][j]);
-        }
-        printf("\n");
-    }
-
+    int r,c;
+    printf("enter row number and column number");
+    scanf("%d%d,&r,&c);
+     int arr[500][500];
+    for(int i=0;i<r;i++)
+          {
+              for(int j=0;j<c;j++)
+              {
+                  printf("Enter %d x %d element",i,j);
+                  scanf("%d",&arr[i][j]);
+              }
+          }
+     printf(" the 2D matrix is =");
+          for(int i=0;i<r;i++)
+          {
+              for(int j=0;j<c;j++)
+              {
+                  printf("%d",arr[i][j]);
+              }
+          }
+          
     return 0;
 }
