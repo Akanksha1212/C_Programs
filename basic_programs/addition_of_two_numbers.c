0c08d72e9ececdc05d2d9c29dd99fd69753361a0
Mohit Khemchandani <42883046+mohitkhemchandani@users.noreply.github.com>
2019-10-15 01:58:59 +0530

    Add files via upload

---
+++ basic_programs/addition_of_two_numbers.c
@@ -0,0 +1,16 @@
+// AUTHOR - Mohit Khemchandani
+// Language - C
+// GITHUB - https://github.com/mohitkhemchandani
+
+#include <stdio.h>
+
+void main()
+{
+int a, b, sum;
+printf("Enter first number =");
+scanf("%d", &a);
+printf("\nEnter second number =");
+scanf("%d", &b);
+sum=a+b;
+printf("Sum of two numbers=%d", sum);
+}
