7ce9916e7e718bff82e64c66d0db7233e8757636
lsb4 <55896135+lsb4@users.noreply.github.com>
2019-10-14 16:22:32 -0300

    Update by lsb4 BubbleSort.c
    I added the option to write the sequence and choose the type of ordination.

--- sorting/Bubble Sort.c
+++ sorting/Bubble Sort.c
@@ -1,28 +1,48 @@
-#include <stdio.h>
-int main()
-{
-    int a[100],i,j,n,temp=0;
-    scanf("%d",&n);
-    for (i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    for(i= 0;i<n;i++)
-    {
-        for (j= 0;j<i; j++)
-        {
-            if (a[i]<a[j])
-            {
-                temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
-            }
-        }
-    }
-    printf("bubble sort");
-    for (i=0;i<n;i++)
-    {
-        printf("%d\n", a[i]);
-    }
-    return 0;
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+
+	int length, i, k, num[1000], aux;
+	char order;
+
+	printf("What is the length of your sequence?\nType here: ");
+	scanf("%d", &length);
+	printf("Type the numbers that you want to order:\n");
+	for(i=0; i<length; i++){
+		scanf("%d", &num[i]);
+	}
+	printf("You want to put in which ordination: crescent(C) or decrescent(D)? ");
+	scanf(" %c", &order);
+
+	if(order=='C' || order=='c'){
+		for(i=0; i<length; i++){
+			for(k=0; k<length; k++){
+				if(num[k]>num[k+1]){
+					aux=num[k];
+					num[k]=num[k+1];
+					num[k+1]=aux;
+				}
+			}
+		}
+		printf("Your sequence in crescent ordination is: ");
+		for(i=0; i<length; i++){
+			printf("%d a ", num[i]);
+		}
+	}else if(order=='D' || order=='d'){
+		for(i=0; i<length; i++){
+			for(k=0; k<length-1; k++){
+				if(num[k]<num[k+1]){
+					aux=num[k];
+					num[k]=num[k+1];
+					num[k+1]=aux;
+				}
+			}
+		}
+		printf("Your sequence in decrescent ordination is: ");
+		for(i=0; i<length; i++){
+			printf("%d ", num[i]);
+		}
+	}
+
+	return 0;
 }
