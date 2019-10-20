418b7b9e1a577a23fab441ce13fbe9bc33caff54
pjaluka13 <53235593+pjaluka13@users.noreply.github.com>
2019-10-20 17:51:49 +0530

    added main function
    accepts data

--- structure/employee.h
+++ structure/employee.h
@@ -1,6 +1,14 @@
 struct Employee
 {
-	struct Address address;
+	char address[100];
 	char name[30];
 	int age;
 };
+int main()
+{
+	struct Employee e;
+	scanf("%d",&e.age);
+	scanf("%s",&e.name);
+	scanf("%s",&e.address);
+	return 0;
+}
