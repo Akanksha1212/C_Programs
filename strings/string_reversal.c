6a43a90b41ed2382d0e4345cff8e60cde0aaeec4
Parth Pandey <55892170+pp2236@users.noreply.github.com>
2020-10-01 00:55:03 +0530

    Update string_reversal.c

--- strings/string_reversal.c
+++ strings/string_reversal.c
@@ -1,18 +1,17 @@
-String reversal using strrev
-
+//reverse string without a function
 #include <stdio.h>
 #include <string.h>
- 
 int main()
 {
-   char array[100];
- 
-   printf("Enter a string: ");
-   gets(array);
- 
-   strrev(array);
- 
-   printf("Reverse of the string: %s", array);
- 
-   return 0;
+   char s[100];
+   scanf("%s",s);
+   int n=strlen(s);
+   for(int i=0;i<n/2;i++){
+       char a=s[i];
+       s[i]=s[n-1-i];
+       s[n-1-i]=a;
+   }
+   printf("%s",s);
+
+    return 0;
 }
