6dbad239af7320129f26f97db45ccdb0a022961c
Eeshan Dutta <53020601+EeshanDutta007@users.noreply.github.com>
2020-10-01 00:14:34 +0530

    Update Bubble Sort.c

--- sorting/Bubble Sort.c
+++ sorting/Bubble Sort.c
@@ -15,29 +15,19 @@ int main(){
 	scanf(" %c", &order);
 
 	if(order=='C' || order=='c'){
-		for(i=0; i<length; i++){
-			for(k=0; k<length; k++){
-				if(num[k]>num[k+1]){
-					aux=num[k];
-					num[k]=num[k+1];
-					num[k+1]=aux;
-				}
-			}
-		}
+		for(i=0; i<length; i++)
+			for(k=0; k<length; k++)
+				if(num[k]>num[k+1])
+					num[k]=num[k]+num[k+1]-(num[k+1]=num[k]);
 		printf("Your sequence in crescent ordination is: ");
 		for(i=0; i<length; i++){
 			printf("%d a ", num[i]);
 		}
 	}else if(order=='D' || order=='d'){
-		for(i=0; i<length; i++){
-			for(k=0; k<length-1; k++){
-				if(num[k]<num[k+1]){
-					aux=num[k];
-					num[k]=num[k+1];
-					num[k+1]=aux;
-				}
-			}
-		}
+		for(i=0; i<length; i++)
+			for(k=0; k<length-1; k++)
+				if(num[k]<num[k+1])
+					num[k]=num[k]+num[k+1]-(num[k+1]=num[k]);
 		printf("Your sequence in decrescent ordination is: ");
 		for(i=0; i<length; i++){
 			printf("%d ", num[i]);
