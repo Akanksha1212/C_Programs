6c79618452e3ffadcab10709efd80da54488a1a9
naveen701526 <70085321+naveen701526@users.noreply.github.com>
2020-10-01 14:26:40 +0530

    Update ternary_search.cpp

--- ternary_search.cpp
+++ ternary_search.cpp
@@ -1,24 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int ternarySearch(int low, int high, int *a, int key)
+int ternarySearch(int low, int high, int *a, int target)
 {
     int mid1 = low + (high - low) / 3;
     int mid2 = high - (high - low) / 3;
 
     if(high>=low)
     {
-        if(key==a[mid1])
+        if(target==a[mid1])
             return mid1;
-        if(key==a[mid2])
+        if(target==a[mid2])
             return mid2;
 
         if(key<a[mid1])
-            return ternarySearch(low, mid1-1, a, key);
+            return ternarySearch(low, mid1-1, a, target);
         else if(key>a[mid2])
-            return ternarySearch(mid2+1, high, a, key);
+            return ternarySearch(mid2+1, high, a, target);
         else
-            return ternarySearch(mid1+1, mid2-1, a, key);
+            return ternarySearch(mid1+1, mid2-1, a, target);
     }
     return -1;
 }
