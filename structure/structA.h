87f89d8a674c784755fcde6c5615419148f39efa
ShahbazCS <72274984+ShahbazCS@users.noreply.github.com>
2020-10-03 01:50:09 +0530

    Update structA.h
    Added one more member to the struct and also added comments.

--- structure/structA.h
+++ structure/structA.h
@@ -5,4 +5,6 @@ struct A
     int number;				// 4 bytes - no padding needed
     short id;				// 2 bytes in memory
 	//char shortPadding[2];   // 2 bytes padding  
-};
+	long id2;			//8 bytes in memory
+};   	  //Notice ; at the end of struct
+//symbol, number, and id are members of struct A
