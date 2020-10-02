970894180d430561b10e62707e6a7023af48d687
SanjayKishorSanil <52646889+SanjayKishorSanil@users.noreply.github.com>
2020-10-02 09:40:34 +0530

    Update factorial.c

--- basic_programs/factorial.c
+++ basic_programs/factorial.c
@@ -1,12 +1,10 @@
 //Factorial of Numbers
 #include"stdio.h"
-int fact(int);
 int main()
 {
 	int n;
 	printf("Enter the number you want to calculate the factorial-->\n");
 	scanf("%d",&n);
-	fact(n);
 	printf("The factorial of the entered number is %d\n",fact(n));
 	return 0;	
 }
