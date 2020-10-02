fcbcd3c8aa0685017a32c3469aed36e51d8baa71
impossibleshado1 <72246040+impossibleshado1@users.noreply.github.com>
2020-10-02 12:42:35 +0530

    Update helloworldd.c

--- helloworldd.c
+++ helloworldd.c
@@ -1,4 +1,15 @@
+/*
+ *
+ SIMPLE C PROGRAM TO PRINT HELLO WORLD!
+ *
+*/
+
+
+
+// Header File [has the information related to input/output functions of C Language]
 #include <stdio.h>
+
+//Main Function
 int main()
 {
    printf("Hello, World!");
